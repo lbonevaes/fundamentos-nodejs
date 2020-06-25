00488c8386e42cb09e187edd2da786bf8452c700
Leandro Bonevaes <lbonevaes@gmail.com>
2020-06-25 14:23:58 -0300

    Create transactions and list with balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO{
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return {income: this.getTotalIncome(), outcome: this.getTotalOutcome(), total: (this.getTotalIncome() - this.getTotalOutcome())};
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+
+  public getTotalIncome(): number {
+    return this.all().reduce(
+      function (total, item){
+        if(item.type === 'income'){
+           return total + item.value;
+        }
+
+        return total;
+      }, 0
+    );
+  }
+
+  public getTotalOutcome(): number {
+    return this.all().reduce(
+      function (total, item){
+        if(item.type === 'outcome'){
+           return total + item.value;
+        }
+
+        return total;
+      }, 0
+    );
   }
 }
 
